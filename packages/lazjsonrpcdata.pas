64622adb058ecb5d4befadbd8ad805cd0681e2cc
Gustavo Carreno <guscarreno@gmail.com>
2021-05-23 15:33:29 +0100

    chore: Created a runtime only package with the lib

---
+++ packages/lazjsonrpcdata.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit lazJSONRPCData;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  LJD.JSON.Utils, LJD.Error, LJD.Request, LJD.Response;
+
+implementation
+
+end.
